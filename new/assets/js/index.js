c49ba944aa215ca5df19acecef52fb44bf47d49c
Jak <jaksawesome10@gmail.com>
2020-03-29 18:03:33 -0400

    I guess darkmode was what was breaking everything

--- new/assets/js/index.js
+++
@@ -1,5 +0,0 @@
-
-function toggleDark(checkbox){
-  document.body.className = checkbox.checked ? "dark" : "";
-  document.getElementById("icon").src = "assets/img/" + (checkbox.checked ? "darkIcon" : "icon") + ".png";
-}
