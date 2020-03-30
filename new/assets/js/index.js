2b8a8bdbb0f437a1e3110f3dd5bac8d4c0e81c34
Jak <jaksawesome10@gmail.com>
2020-03-30 15:18:17 -0400

    wallpaper buttons don't work yet

---
+++ new/assets/js/index.js
@@ -0,0 +1,22 @@
+
+/*
+
+function downloadWallpaper(wallpaper){
+  var a = $('<a>')
+    .attr('href', 'assets/img/wallpapers/' + wallpaper + '.png')
+    .attr('download', wallpaper + '.png')
+    .appendTo('body');
+  a[0].click();
+  a.remove();
+}
+
+function downloadWallpaperSource(wallpaper){
+  var a = $('<a>')
+    .attr('href', 'assets/img/wallpapers/CodeBlock.xcf')
+    .attr('download', wallpaper + '.xcf')
+    .appendTo('body');
+  a[0].click();
+  a.remove();
+}
+
+*/
