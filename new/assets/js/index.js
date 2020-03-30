45becee72c4803fad1119fd84569fe300e0861a5
Jak <jaksawesome10@gmail.com>
2020-03-30 15:42:33 -0400

    code efficiency

--- new/assets/js/index.js
+++ new/assets/js/index.js
@@ -1,17 +1,19 @@
 
 function downloadWallpaper(wallpaper){
-  var a = $('<a>')
-    .attr('href', 'assets/img/wallpapers/' + wallpaper + ($(window).width() < 850 ? 'Mobile' : '') + '.png')
-    .attr('download', wallpaper + ($(window).width() < 850 ? 'Mobile' : '') + '.png')
+  wallpaper += ($(window).width() < 850 ? 'Mobile' : '');
+  let a = $('<a>')
+    .attr('href', 'assets/img/wallpapers/' + wallpaper + '.png')
+    .attr('download', wallpaper + '.png')
     .appendTo('body');
   a[0].click();
   a.remove();
 }
 
 function downloadWallpaperSource(wallpaper){
-  var a = $('<a>')
-    .attr('href', 'assets/img/wallpapers/' + wallpaper + ($(window).width() < 850 ? 'Mobile' : '') + '.xcf')
-    .attr('download', wallpaper + ($(window).width() < 850 ? 'Mobile' : '') + '.xcf')
+  wallpaper += ($(window).width() < 850 ? 'Mobile' : '');
+  let a = $('<a>')
+    .attr('href', 'assets/img/wallpapers/' + wallpaper + '.xcf')
+    .attr('download', wallpaper + '.xcf')
     .appendTo('body');
   a[0].click();
   a.remove();
