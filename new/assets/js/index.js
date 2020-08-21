0d23ccf360e2afac71e78d1048d6f9a3b118936c
Jak <jaksawesome10@gmail.com>
2020-08-20 23:37:11 -0400

    that's all for today

--- new/assets/js/index.js
+++ new/assets/js/index.js
@@ -1,9 +1,11 @@
 
 // Onload Tasks
-
 setTimeout(function(){
     document.querySelectorAll('section:not(#home), footer').forEach(current => {
         current.style.display = "block"; 
     });
-}, 3500);
+}, 2000);
+
+
+
 
